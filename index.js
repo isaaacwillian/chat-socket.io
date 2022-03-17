2e0878a126eeb24a6bda3c11e7b33f76e8291e52
Isaac Willian <isaaacwillian@gmail.com>
2022-03-17 17:06:19 -0300

    Feito a conexão, já envia msgs para todos

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const socketIo = require("socket.io");
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log("Server running");
+});
+
+const io = socketIo(server);
+
+const messages = [];
+
+io.on("connection", (socket) => {
+  console.log("new connection");
+  socket.emit("update_messages", messages);
+
+  socket.on("new_message", (data) => {
+    messages.push(data.msg);
+
+    io.emit("update_messages", messages);
+  });
 });
